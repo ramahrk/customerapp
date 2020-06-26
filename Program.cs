1403df80a87503afb5403ad518433e1b82e7676c
ramah <ramahrk@gmail.com>
2020-06-26 23:52:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+ 
+namespace Whizlabapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+			Console.WriteLine("This is a change");
+        }
+ 
+      
+    }
+}
+ 
